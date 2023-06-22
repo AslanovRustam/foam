3b0fbfa339ef2c00e4082144a6288cbd80ae21cc
Aslanov Rustam <aslanovrg@gmail.com>
2023-06-22 18:24:12 +0300

    add footer, form

---
+++ src/components/Supply/Supply.jsx
@@ -0,0 +1,5 @@
+import s from "./supply.module.css";
+
+export default function Supply() {
+  return <div>Supply</div>;
+}
--- src/pages/SharedLayaut/SharedLayaut.jsx
+++ src/pages/SharedLayaut/SharedLayaut.jsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import { Suspense } from "react";
+import Container from "../../components/Container/Container";
 
 export default function SharedLayaut() {
   return (
     <>
-      <Header />
+      <Container>
+        <Header />
+      </Container>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
