586afe291316753f9e1fbd516156b234aa725111
Aslanov Rustam <aslanovrg@gmail.com>
2023-06-28 17:47:13 +0300

    upd products section

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import Container from "../../components/Container/Container";
+import s from "./productPage.module.css";
 import Variants from "../../components/Variants/Variants";
+import ProductList from "../../components/ProductList/ProductList";
 
 export default function ProductsPage() {
   return (
-    // <Container>
-    <Variants />
-    // </Container>
+    <div className={s.container}>
+      <Variants />
+      <ProductList />
+    </div>
   );
 }
